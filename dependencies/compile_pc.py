384b9c278989b3ba1bc3cb6a850eed6ac94f1515
Monaco-a-Knox <58667780+Monaco-a-Knox@users.noreply.github.com>
2026-03-30 21:45:25 -0300

    Update compile_pc.py

--- dependencies/compile_pc.py
+++ dependencies/compile_pc.py
@@ -27,7 +27,7 @@ def compile():
     
     # nome do arquivo de destino
     # IMPORTANTE: o nome do arquivo, caso modificado, precisa também ser modificado nos scripts do Github Actions, sob a pasta .github/workflows neste repositório
-    zip_args = f"KT.Kaziklu.Bey.~Entrevista.com.o.Vampiro~.7z {patch_folder}"
+    zip_args = f"[KT].Kaziklu.Bey.~Entrevista.com.o.Vampiro~.7z {patch_folder}"
     run([r'dependencies/7za.exe', 'a'] + shlex.split(zip_args))
 
 stringtool()
